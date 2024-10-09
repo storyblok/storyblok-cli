7e448ae0fe73e4d2b6cd8d4f5dd3658abf751112
alvarosabu <alvaro.saburido@gmail.com>
2024-10-09 17:44:00 +0200

    feat: narrow the typesafe of constants

--- src/constants.ts
+++ src/constants.ts
@@ -1,41 +1,41 @@
-export const commands: Record<string, string> = {
+export const commands = {
   LOGIN: 'login',
   LOGOUT: 'logout',
-}
+} as const
 
-export const regions: Record<string, string> = {
+export const regions = {
   EU: 'eu',
   US: 'us',
   CN: 'cn',
   CA: 'ca',
   AP: 'ap',
-}
+} as const
 
-export const regionsDomain: Record<string, string> = {
+export const regionsDomain = {
   eu: 'api.storyblok.com',
   us: 'api-us.storyblok.com',
   cn: 'app.storyblokchina.cn',
   ca: 'api-ca.storyblok.com',
   ap: 'api-ap.storyblok.com',
-}
+} as const
 
-export const managementApiRegions: Record<string, string> = {
+export const managementApiRegions = {
   eu: 'mapi.storyblok.com',
   us: 'mapi-us.storyblok.com',
   cn: 'mapi.storyblokchina.cn',
   ca: 'mapi-ca.storyblok.com',
   ap: 'mapi-ap.storyblok.com',
-}
+} as const
 
-export const regionNames: Record<string, string> = {
+export const regionNames = {
   eu: 'Europe',
   us: 'United States',
   cn: 'China',
   ca: 'Canada',
   ap: 'Australia',
-}
+} as const
 
-export const DEFAULT_AGENT: Record<string, string> = {
+export const DEFAULT_AGENT = {
   SB_Agent: 'SB-CLI',
   SB_Agent_Version: process.env.npm_package_version || '4.x',
-}
+} as const
