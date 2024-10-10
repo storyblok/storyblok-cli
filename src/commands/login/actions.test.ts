64ef7dbb6e93c08ebad953c60e3a61154f3fafbb
alvarosabu <alvaro.saburido@gmail.com>
2024-10-10 16:37:54 +0200

    chore(tests): type assertion for mocks

--- src/commands/login/actions.test.ts
+++ src/commands/login/actions.test.ts
@@ -1,3 +1,4 @@
+import type { Mock } from 'vitest'
 import { describe, expect, it, vi } from 'vitest'
 import { loginWithEmailAndPassword, loginWithOtp, loginWithToken } from './actions'
 import { ofetch } from 'ofetch'
@@ -7,6 +8,8 @@ vi.mock('ofetch', () => ({
   ofetch: vi.fn(),
 }))
 
+const mockedFetch = ofetch as unknown as Mock
+
 describe('login actions', () => {
   beforeEach(() => {
     vi.clearAllMocks()
@@ -15,7 +18,7 @@ describe('login actions', () => {
   describe('loginWithToken', () => {
     it('should login successfully with a valid token', async () => {
       const mockResponse = { data: 'user data' }
-      ofetch.mockResolvedValue(mockResponse)
+      mockedFetch.mockResolvedValue(mockResponse)
 
       const result = await loginWithToken('valid-token', 'eu')
       expect(result).toEqual(mockResponse)
@@ -26,7 +29,7 @@ describe('login actions', () => {
         response: { status: 401 },
         data: { error: 'Unauthorized' },
       }
-      ofetch.mockRejectedValue(mockError)
+      mockedFetch.mockRejectedValue(mockError)
 
       await expect(loginWithToken('invalid-token', 'eu')).rejects.toThrow(
         new Error(`The token provided ${chalk.bold('inva*********')} is invalid: ${chalk.bold('401 Unauthorized')}
@@ -37,7 +40,7 @@ describe('login actions', () => {
 
     it('should throw a network error if response is empty (network)', async () => {
       const mockError = new Error('Network error')
-      ofetch.mockRejectedValue(mockError)
+      mockedFetch.mockRejectedValue(mockError)
 
       await expect(loginWithToken('any-token', 'eu')).rejects.toThrow(
         'No response from server, please check if you are correctly connected to internet',
@@ -48,7 +51,7 @@ describe('login actions', () => {
   describe('loginWithEmailAndPassword', () => {
     it('should login successfully with valid email and password', async () => {
       const mockResponse = { data: 'user data' }
-      ofetch.mockResolvedValue(mockResponse)
+      mockedFetch.mockResolvedValue(mockResponse)
 
       const result = await loginWithEmailAndPassword('email@example.com', 'password', 'eu')
       expect(result).toEqual(mockResponse)
@@ -56,7 +59,7 @@ describe('login actions', () => {
 
     it('should throw a generic error for login failure', async () => {
       const mockError = new Error('Network error')
-      ofetch.mockRejectedValue(mockError)
+      mockedFetch.mockRejectedValue(mockError)
 
       await expect(loginWithEmailAndPassword('email@example.com', 'password', 'eu')).rejects.toThrow(
         'Error logging in with email and password',
@@ -67,7 +70,7 @@ describe('login actions', () => {
   describe('loginWithOtp', () => {
     it('should login successfully with valid email, password, and otp', async () => {
       const mockResponse = { data: 'user data' }
-      ofetch.mockResolvedValue(mockResponse)
+      mockedFetch.mockResolvedValue(mockResponse)
 
       const result = await loginWithOtp('email@example.com', 'password', '123456', 'eu')
       expect(result).toEqual(mockResponse)
@@ -75,7 +78,7 @@ describe('login actions', () => {
 
     it('should throw a generic error for login failure', async () => {
       const mockError = new Error('Network error')
-      ofetch.mockRejectedValue(mockError)
+      mockedFetch.mockRejectedValue(mockError)
 
       await expect(loginWithOtp('email@example.com', 'password', '123456', 'eu')).rejects.toThrow(
         'Error logging in with email, password and otp',
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,5 +1,6 @@
 import { fileURLToPath } from 'node:url'
 import { dirname } from 'pathe'
+import type { RegionCode } from '../constants'
 import { regions } from '../constants'
 
 export * from './error'
@@ -7,7 +8,7 @@ export * from './konsola'
 export const __filename = fileURLToPath(import.meta.url)
 export const __dirname = dirname(__filename)
 
-export function isRegion(value: string): value is keyof typeof regions {
+export function isRegion(value: RegionCode): value is RegionCode {
   return Object.values(regions).includes(value)
 }
 
