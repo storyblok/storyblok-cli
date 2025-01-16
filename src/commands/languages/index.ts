4862d0dcc4a2fcc1ed7f55c4917e7dfeb8b7c04f
alvarosabu <alvaro.saburido@gmail.com>
2025-01-16 14:36:11 +0100

    feat: add alias for languages command
    - Introduced an alias 'lang' for the 'languages' command to enhance usability and streamline command input.

--- src/commands/languages/index.ts
+++ src/commands/languages/index.ts
@@ -10,6 +10,7 @@ const program = getProgram() // Get the shared singleton instance
 
 export const languagesCommand = program
   .command('languages')
+  .alias('lang')
   .description(`Manage your space's languages`)
   .option('-s, --space <space>', 'space ID')
   .option('-p, --path <path>', 'path to save the file. Default is .storyblok/languages')
