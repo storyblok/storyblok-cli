332ad980e93dc23e0e033e70897810a785a3cf62
Alvaro Saburido <alvaro.saburido@gmail.com>
2025-01-21 11:00:29 +0100

    Update src/commands/components/index.ts
    Co-authored-by: Edoardo Dusi <edo@edoardodusi.com>

--- src/commands/components/index.ts
+++ src/commands/components/index.ts
@@ -10,6 +10,7 @@ const program = getProgram() // Get the shared singleton instance
 
 export const componentsCommand = program
   .command('components')
+  .alias('comp')
   .description(`Manage your space's block schema`)
   .option('-s, --space <space>', 'space ID')
   .option('-p, --path <path>', 'path to save the file. Default is .storyblok/components')
