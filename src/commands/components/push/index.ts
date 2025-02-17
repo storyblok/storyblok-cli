0c0308aacdc8f2343ba5212a7d7eae9b42d5e4b8
alvarosabu <alvaro.saburido@gmail.com>
2025-02-17 08:52:06 +0100

    refactor: remove debug logging in push command
    - Deleted unnecessary console.log statement in handleComponents method
    - Cleaned up verbose logging of internal state during component processing

--- src/commands/components/push/index.ts
+++ src/commands/components/push/index.ts
@@ -108,22 +108,6 @@ componentsCommand
         component => !whitelistResults.processedComponentNames.has(component.name),
       );
 
-      console.log('handleComponents', {
-        space,
-        password,
-        region,
-        spaceData: {
-          ...spaceData,
-          components: remainingComponents,
-        },
-        groupsUuidMap: new Map([...whitelistResults.groupsUuidMap, ...groupsResults.uuidMap]), // Merge both group maps
-        tagsIdMaps: new Map([...whitelistResults.tagsIdMap, ...tagsResults.idMap]), // Merge both tag maps
-        componentNameMap: whitelistResults.componentNameMap,
-        processedGroupUuids: whitelistResults.processedGroupUuids,
-        processedTagIds: whitelistResults.processedTagIds,
-        processedComponentNames: whitelistResults.processedComponentNames,
-      });
-
       const componentsResults = await handleComponents({
         space,
         password,
