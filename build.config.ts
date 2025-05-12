0631dfc881b663c782ef63d6499e0738dcf32e38
alvarosabu <alvaro.saburido@gmail.com>
2025-05-12 08:35:15 +0200

    chore: remove pathe from externals
    - Removed 'pathe' from the externals in build.config.ts to streamline the build process.
    - Added 'pathe' as a dependency in package.json to ensure it is included in the project, improving module resolution and functionality.

--- build.config.ts
+++ build.config.ts
@@ -3,6 +3,5 @@ import { defineBuildConfig } from 'unbuild';
 export default defineBuildConfig({
   declaration: true,
   entries: ['./src/index'],
-  externals: ['pathe'],
   failOnWarn: false,
 });
