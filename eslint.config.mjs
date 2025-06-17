5409d40c291e24993388004011b3103111fddd8f
alvarosabu <alvaro.saburido@gmail.com>
2025-06-17 08:59:19 +0200

    chore: disable max-statements-per-line rule in ESLint configuration

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,5 +3,6 @@ import { storyblokLintConfig } from '@storyblok/eslint-config';
 export default storyblokLintConfig({
   rules: {
     'no-console': 'off',
+    'style/max-statements-per-line': 'off',
   },
 });
